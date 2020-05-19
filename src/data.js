d7522e213657c9b3469c9e824607d4c4eb7c0a0d
colekins <colekins@gmail.com>
2020-05-19 19:44:03 -0400

    Update Dust Etc project

--- src/data.js
+++ src/data.js
@@ -1,74 +1,85 @@
 const data = {
-    "projects" : [
-        {
-            "title": "Sunrise Presidential Scorecard",
-            "date": "December 2019",
-            "description": ["Presidential scorecard web app. Scalable React UI dynamically builds all tables and modal content. Custom parser script translates CSV data into consumable JSON.", 
-                            "Led React development in coordination with a small of team of volunteer designers & developers.",
-                            'React, Javascript, Node, CSS, CSV parser, Travis CI, Netlify CD.'],
-            "live": "https://scorecard.sunrisemovement.org",
-            "repo": "https://github.com/sunrisemovement/scorecard",
-            "screenshot" : "https://i.imgur.com/DSxd1dw.png"
-        },
-        {
-            "title": "VoteDems",
-            "date": "August 2019",
-            "description": ["Simple POC app to compare the plans of the 2020 Presidential candidates.",
-                            "Includes Netflix-style horizontal scrolling cards and a CSS theme toggle.", 
-                            'React, Javascript, Node, CSS.'],
-            "live": "https://votedems.info",
-            "repo": "https://github.com/colekins/votedems.info",
-            "screenshot" : "https://i.imgur.com/PyOhcwO.png"
-        },
-        // {
-        //     "title": "Pile",
-        //     "date": "March 2019",
-        //     "description": ["Website for rock band from Boston, MA. Headless CMS solution using the band's preexisting Tumblr blog.", 
-        //                     'HTML, CSS, Bootstrap, Tumblr API.'],
-        //     "live": "https://pilemusic.com",
-        //     "repo": "https://github.com/colekins/pile",
-        //     "screenshot" : "https://i.imgur.com/iwch7jw.png"
-        // },
-        {
-            "title": "Dust, Etc.",
-            "date": "March 2018",
-            "description": ['Simple static website for DIY record label in Boston, MA.', 
-                            'HTML, CSS, Bootstrap, Bandcamp embeds.'],
-            "live": "https://dustetc.com",
-            "repo": "https://github.com/colekins/dust_etc",
-            "screenshot" : "https://i.imgur.com/0disyUC.png"
-        },
-        {
-            "title": "BookStack",
-            "date": "December 2017",
-            "description": ['An app for bookworms. BookStack allows users to track, organize, and maintain their book collections. From each entry, a user can add pertinent notes on the book, or add it to their "read next" list.', 
-                            'Google Books API integration for dynamic book covers. Random New Yorker cartoon at welcome page. Dynamic links for Amazon & Goodreads.', 
-                            'HTML, CSS, Bootstrap, Javascript, jQuery, AJAX, Handlebars, Ruby on Rails, PostgreSQL.'],
-            "live": "https://colekins.github.io/bookstack",
-            "repo": "https://github.com/colekins/bookstack",
-            "screenshot" : "https://i.imgur.com/BdEUUDZ.png"
-        },
-        {
-            "title": "MyVinyl",
-            "date": "October 2017",
-            "description": ['An app for record collectors. MyVinyl allows users to digitally store their record collections, and add relevant pressing notes to each entry.', 
-                            'Last.fm API integration for album art.', 
-                            'HTML, CSS, Bootstrap, Javascript, jQuery, AJAX, Handlebars, Ruby on Rails, PostgreSQL.'],
-            "live": "https://colekins.github.io/myvinyl",
-            "repo": "https://github.com/colekins/myvinyl",
-            "screenshot": "https://i.imgur.com/Iq3gSBd.png"
-        }
-        // {
-        //     "title": "Tic-Tac-Toe",
-        //     "date": "September 2017",
-        //     "description": ['Classic game of Tic-Tac-Toe. Game logic built with Javascript.', 
-        //                     'HTML, CSS, Javascript, jQuery, AJAX.'
-        //                 ],
-        //     "live": "https://colekins.github.io/tictactoe",
-        //     "repo": "https://github.com/colekins/tictactoe",
-        //     "screenshot": "https://i.imgur.com/uQ16U4Z.png"
-        // }
-    ]
+  projects: [
+    {
+      title: "Sunrise Presidential Scorecard",
+      date: "December 2019",
+      description: [
+        "Presidential scorecard web app. Scalable React UI dynamically builds all tables and modal content. Custom parser script translates CSV data into consumable JSON.",
+        "Led React development in coordination with a small of team of volunteer designers & developers.",
+        "React, Javascript, Node, CSS, CSV parser, Travis CI, Netlify CD.",
+      ],
+      live: "https://scorecard.sunrisemovement.org",
+      repo: "https://github.com/sunrisemovement/scorecard",
+      screenshot: "https://i.imgur.com/DSxd1dw.png",
+    },
+    {
+      title: "Dust, Etc.",
+      date: "May 2020",
+      description: [
+        "Static website with CMS for DIY record label in Portland, ME.",
+        "Vue, Netlify CMS, Javascript, CSS, BootstrapVue.",
+      ],
+      live: "https://dustetc.com",
+      repo: "https://github.com/colekins/dust-etc-vue",
+      screenshot: "https://i.imgur.com/nhgswnO.png",
+    },
+    {
+      title: "VoteDems",
+      date: "August 2019",
+      description: [
+        "Simple POC app to compare the plans of the 2020 Presidential candidates.",
+        "Includes Netflix-style horizontal scrolling cards and a CSS theme toggle.",
+        "React, Javascript, Node, CSS.",
+      ],
+      live: "https://votedems.info",
+      repo: "https://github.com/colekins/votedems.info",
+      screenshot: "https://i.imgur.com/PyOhcwO.png",
+    },
+    // {
+    //     "title": "Pile",
+    //     "date": "March 2019",
+    //     "description": ["Website for rock band from Boston, MA. Headless CMS solution using the band's preexisting Tumblr blog.",
+    //                     'HTML, CSS, Bootstrap, Tumblr API.'],
+    //     "live": "https://pilemusic.com",
+    //     "repo": "https://github.com/colekins/pile",
+    //     "screenshot" : "https://i.imgur.com/iwch7jw.png"
+    // },
+
+    {
+      title: "BookStack",
+      date: "December 2017",
+      description: [
+        'An app for bookworms. BookStack allows users to track, organize, and maintain their book collections. From each entry, a user can add pertinent notes on the book, or add it to their "read next" list.',
+        "Google Books API integration for dynamic book covers. Random New Yorker cartoon at welcome page. Dynamic links for Amazon & Goodreads.",
+        "HTML, CSS, Bootstrap, Javascript, jQuery, AJAX, Handlebars, Ruby on Rails, PostgreSQL.",
+      ],
+      live: "https://colekins.github.io/bookstack",
+      repo: "https://github.com/colekins/bookstack",
+      screenshot: "https://i.imgur.com/BdEUUDZ.png",
+    },
+    {
+      title: "MyVinyl",
+      date: "October 2017",
+      description: [
+        "An app for record collectors. MyVinyl allows users to digitally store their record collections, and add relevant pressing notes to each entry.",
+        "Last.fm API integration for album art.",
+        "HTML, CSS, Bootstrap, Javascript, jQuery, AJAX, Handlebars, Ruby on Rails, PostgreSQL.",
+      ],
+      live: "https://colekins.github.io/myvinyl",
+      repo: "https://github.com/colekins/myvinyl",
+      screenshot: "https://i.imgur.com/Iq3gSBd.png",
+    },
+    // {
+    //     "title": "Tic-Tac-Toe",
+    //     "date": "September 2017",
+    //     "description": ['Classic game of Tic-Tac-Toe. Game logic built with Javascript.',
+    //                     'HTML, CSS, Javascript, jQuery, AJAX.'
+    //                 ],
+    //     "live": "https://colekins.github.io/tictactoe",
+    //     "repo": "https://github.com/colekins/tictactoe",
+    //     "screenshot": "https://i.imgur.com/uQ16U4Z.png"
+    // }
+  ],
 };
 
 export default data;
