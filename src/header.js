4c4db84aecbc73ff2b291a44a5ed9bf808ec374b
colekins <colekins@gmail.com>
2021-07-14 15:58:55 -0400

    Add web

--- src/header.js
+++ src/header.js
@@ -1,37 +1,37 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 const Header = () => {
   return (
-    <div className="">
-      <div className="row">
+    <div className=''>
+      <div className='row'>
         <img
-          className="profile"
-          alt="profile"
-          src="https://i.imgur.com/3GuSFbJ.jpg"
+          className='profile'
+          alt='profile'
+          src='https://i.imgur.com/3GuSFbJ.jpg'
         />
-        <div className="col-md-7 intro-box">
-          <h1 className="name">Cole Kinsler</h1>
-          <p className="intro">
-            Hi there, I'm a developer located in Portland, Maine.
+        <div className='col-md-7 intro-box'>
+          <h1 className='name'>Cole Kinsler</h1>
+          <p className='intro'>
+            Hi there, I'm a web developer located in Portland, Maine.
           </p>
-          <p className="intro">
+          <p className='intro'>
             I like working with Javascript, React, Vue, HTML5, CSS3, Sass and
             Node. I'm especially interested in emerging front end frameworks,
             GraphQL, and CSS Grid at the moment.
           </p>
-          <div className="actions">
+          <div className='actions'>
             <a
-              href="mailto:colekins@gmail.com?Subject=Hi there"
-              rel="noopener noreferrer"
-              target="_blank"
+              href='mailto:colekins@gmail.com?Subject=Hi there'
+              rel='noopener noreferrer'
+              target='_blank'
             >
               Email
             </a>
             <a
-              href="https://github.com/colekins/"
-              rel="noopener noreferrer"
-              target="_blank"
+              href='https://github.com/colekins/'
+              rel='noopener noreferrer'
+              target='_blank'
             >
               GitHub
             </a>
